fda4f8fc18953b1629e75ab5d26edbfe3ddeab2c
AnastasiaKondr <nk-news@mail.ru>
2023-04-12 23:08:54 +0500

    first task maybe done

--- solution.py
+++ solution.py
@@ -1,14 +1,29 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy.stats import norm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 483660375 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Расчет вероятности успеха на контрольной выборке
+    p1 = x_success / x_cnt
+
+    # Расчет вероятности успеха на тестовой выборке
+    p2 = y_success / y_cnt
+
+    # Расчет статистики Z-критерия
+    z = (p2 - p1) / math.sqrt(p1 * (1 - p1) / x_cnt + p2 * (1 - p2) / y_cnt)
+
+    # Расчет критического значения для заданного уровня значимости
+    alpha = 0.08
+    z_alpha = abs(norm.ppf(alpha / 2))
+
+    # Сравнение статистики с критическим значением и принятие решения
+    if abs(z) > z_alpha:
+        return True  # Отклоняем нулевую гипотезу
+    else:
+        return False  # Не отклоняем нулевую гипотезу
